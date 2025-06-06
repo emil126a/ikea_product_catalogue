180630855f8497cdfe03809989c79250deba434a
Emil Azizov <azizov.emil@gmail.com>
2025-06-06 20:00:48 +0200

    refactor: optimize ProductController with OpenAPI annotations and validation

--- backend/src/main/java/ikea/product/demo/controller/ProductController.java
+++ backend/src/main/java/ikea/product/demo/controller/ProductController.java
@@ -12,12 +12,10 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-
 import jakarta.validation.Valid;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Sort;
