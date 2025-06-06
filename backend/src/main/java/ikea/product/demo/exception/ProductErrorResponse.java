aaecf1252874aaa5490a1f1e4d52f0784ba74c6b
Emil Azizov <azizov.emil@gmail.com>
2025-06-06 19:08:52 +0200

    refactor: removed redundant getters and setters in ProductErrorResponse using Lombok

--- backend/src/main/java/ikea/product/demo/exception/ProductErrorResponse.java
+++ backend/src/main/java/ikea/product/demo/exception/ProductErrorResponse.java
@@ -1,22 +1,9 @@
 package ikea.product.demo.exception;
 
+import lombok.Data;
+
+@Data
 public class ProductErrorResponse {
     private int status;
     private String message;
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public int getStatus() {
-        return status;
-    }
-
-    public void setStatus(int status) {
-        this.status = status;
-    }
 }
